14195d2c23af5b0adea0ac09b788e4d1dbddd451
imwilsonxu <im.wilson.xu@gmail.com>
2012-09-06 17:47:59 +0800

    can post tweets.

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source :rubygems
 gem 'sinatra'
 gem "haml"
 gem "sass"
+gem 'oauth'
+gem 'twitter'
 gem 'weibo_2'
 gem 'time-ago-in-words'
--- fabuniao.rb
+++ fabuniao.rb
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 
 require 'rubygems'
+require 'oauth'
+require 'twitter'
 require 'weibo_2'
 require 'time-ago-in-words'
 
-%w(rubygems bundler).each { |dependency| require dependency }
-Bundler.setup
 %w(sinatra haml sass).each { |dependency| require dependency }
 enable :sessions
-#use Rack::Session::Pool, :expire_after => 2592000
 
 WeiboOAuth2::Config.api_key = ENV['FABUNIAO_API_KEY']
 WeiboOAuth2::Config.api_secret = ENV['FABUNIAO_API_SECRET']
 WeiboOAuth2::Config.redirect_uri = ENV['FABUNIAO_REDIRECT_URI']
+
+$twitter_callback_url = "http://127.0.0.1/twitter_callback"
+$consumer = OAuth::Consumer.new(
+    ENV['FABUNIAO_TWITTER_CONSUMER_KEY'],
+    ENV['FABUNIAO_TWITTER_CONSUMER_SECRET'], 
+    :site => 'http://api.twitter.com')
+
+Twitter.configure do |config|
+    config.consumer_key = ENV['FABUNIAO_TWITTER_CONSUMER_KEY']
+    config.consumer_secret = ENV['FABUNIAO_TWITTER_CONSUMER_SECRET']
+end
 
 # Logger
 use Rack::Logger
@@ -20,7 +30,7 @@ $logger = Logger.new('logs/fabuniao.log')
 $logger.level = Logger::DEBUG
 
 before do
-    $logger.debug(ENV['FABUNIAO_API_KEY'])
+    $logger.debug('------------------------------------')
 end
 
 get '/' do
@@ -32,7 +42,7 @@ get '/' do
 
         unless token.validated?
             reset_session
-            redirect '/connect'
+            redirect '/weibo'
             return
         end
     end
@@ -43,12 +53,12 @@ get '/' do
     haml :index
 end
 
-get '/connect' do
+get '/weibo' do
     client = WeiboOAuth2::Client.new
     redirect client.authorize_url
 end
 
-get '/callback' do
+get '/weibo_callback' do
     client = WeiboOAuth2::Client.new
     access_token = client.auth_code.get_token(params[:code].to_s)
     session[:test] = access_token.params["uid"]
@@ -56,6 +66,26 @@ get '/callback' do
     session[:access_token] = access_token.token
     session[:expires_at] = access_token.expires_at
     @user = client.users.show_by_uid(session[:uid].to_i)
+    redirect '/'
+end
+
+get '/twitter' do
+    $request_token = $consumer.get_request_token(:oauth_callback => "http://127.0.0.1/twitter_callback")
+    $logger.debug("request_token")
+    $logger.debug($request_token)
+    session[:request_token] = $request_token
+    redirect $request_token.authorize_url(:oauth_callback => "http://127.0.0.1/twitter_callback")
+end
+
+get '/twitter_callback' do
+    $access_token = session[:request_token].get_access_token
+    $logger.debug("access_token")
+    $logger.debug($access_token)
+    twitter_client = Twitter::Client.new(
+        :oauth_token => $access_token.token,
+        :oauth_token_secret => $access_token.secret,
+    )
+    twitter_client.update("Testing via twiiter api.")
     redirect '/'
 end
 
